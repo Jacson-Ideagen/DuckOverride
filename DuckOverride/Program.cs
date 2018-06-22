4f38243022ddf1d75a3db62fab3e2d567159bb2b
Jacson Curtis <jacson.curtis@ideagenplc.com>
2018-06-22 16:09:42 +0100

    Initial Commit

---
+++ DuckOverride/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DuckOverride
+{
+    class Program
+    {
+        public bool Running = true;
+
+        static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.Run();
+        }
+
+        public void Run()
+        {
+            var quack = new Duck("Sir Quackalot", "Mallard", 100, 17);
+        }
+    }
+}
