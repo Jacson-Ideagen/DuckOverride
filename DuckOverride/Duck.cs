753485a8802752be0f24a049456712f5d2386163
Jacson Curtis <jacson.curtis@ideagenplc.com>
2018-06-29 11:30:22 +0100

    overriden equality & operators, implemented IComparable

--- DuckOverride/Duck.cs
+++ DuckOverride/Duck.cs
@@ -1,18 +1,69 @@
-﻿namespace DuckOverride
-{
-    public class Duck
-    {
-        public string Name { get; }
-        public string Type { get; }
-        public int WeightInGrams { get; }
-        public int AgeInMonths { get; }
+﻿using System;
 
+namespace DuckOverride
+{
+    public class Duck : IComparable<Duck>
+    {
         public Duck(string name, string type, int weightInGrams, int ageInMonths)
         {
             Name = name;
             Type = type;
             WeightInGrams = weightInGrams;
             AgeInMonths = ageInMonths;
+
+        }
+
+        public string Name { get; }
+
+        public string Type { get; }
+
+        public int WeightInGrams { get; }
+
+        public int AgeInMonths { get; }
+
+        public override int GetHashCode()
+        {
+            var Hash = WeightInGrams;
+            Hash = Hash * 17 + Name.GetHashCode();
+            Hash = Hash * 17 + Type.GetHashCode();
+            Hash = Hash * 17 + AgeInMonths.GetHashCode();
+            return Hash;
+        }
+
+        private int GetNamelessHashCode()
+        {
+            var Hash = WeightInGrams;
+            Hash = Hash * 17 + Type.GetHashCode();
+            Hash = Hash * 17 + AgeInMonths.GetHashCode();
+            return Hash;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            try
+            {
+                return GetNamelessHashCode().Equals(((Duck)obj).GetNamelessHashCode());
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public int CompareTo(Duck other)
+        {
+            return WeightInGrams.CompareTo(other.WeightInGrams);
+        }
+
+        public static bool operator ==(Duck duckLeft, object duckRight)
+        {
+            return Equals(duckLeft, duckRight);
+        }
+
+        public static bool operator !=(Duck duckLeft, object duckRight)
+        {
+            return !Equals(duckLeft, duckRight);
         }
     }
 }
--- DuckOverride/Program.cs
+++ DuckOverride/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DuckOverride
 {
@@ -14,7 +15,36 @@ namespace DuckOverride
 
         public void Run()
         {
+            //ducks
             var quack = new Duck("Sir Quackalot", "Mallard", 100, 17);
+            var qq = new Duck("Quacknotsomuch", "Mallard", 100, 17);
+            var nq = new Duck("Sir Quackalot", "Mallard", 101, 17);
+
+            //testing equality overrides
+            Console.WriteLine(quack == qq);//Expected True
+            Console.WriteLine(quack != qq);//Expected False
+            Console.WriteLine(quack == nq);//Expected False
+            Console.WriteLine(quack != nq);//Expected True
+
+            //Sorting a list of Ducks into order
+            var x = new List<Duck>();
+            x.Add(quack);
+            x.Add(qq);
+            x.Add(nq);
+
+            x.Sort();
+
+            //Using the Duck as the key in a Dictionary
+            var ducktionary = new Dictionary<Duck, int>();
+            foreach (var duck in x)
+            {
+                ducktionary.Add(duck, duck.GetHashCode());
+            }
+
+
+            //END
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
         }
     }
 }
